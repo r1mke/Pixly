a9dd953fc2e00e35503d3236f7b6053b87c5af0a
Kerim Begic <begick89@gmail.com>
2024-11-09 00:05:44 +0100

    task-20

---
+++ frontend/src/app/features/shared/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,46 @@
+import { AfterViewInit, Component, OnInit} from '@angular/core';
+import { CommonModule} from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { WindowSizeService } from '../../../../services/window-size.service';
+import { Renderer2,ElementRef } from '@angular/core';
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [CommonModule, RouterLink, FormsModule],
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css']
+})
+export class NavBarComponent  implements OnInit, AfterViewInit{
+  isScrolled: boolean = false;
+  windowWidth: number = 0;
+  user: boolean = true;
+  menuOpen: boolean = false;  
+
+  constructor(private windowSizeService: WindowSizeService,private router: Router, private renderer: Renderer2) {}
+
+  ngOnInit(): void {
+    this.windowSizeService.data$.subscribe((w)=> {
+      this.windowWidth = w;
+
+      this.menuOpen = this.windowWidth >= 900;
+      console.log(this.menuOpen, this.windowWidth);
+
+    })
+    }
+
+  ngAfterViewInit() {
+
+  }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  goToHome() {
+    this.router.navigate(['/']);
+  }
+ 
+
+}
