f0ed32f2a38a5dcd5e9c0deef89a0be34bd9e152
Kerim Begic <begick89@gmail.com>
2024-12-03 15:23:56 +0100

    task-69 added generate-image-page

---
+++ frontend/src/app/features/public/pages/generate-image-page/generate-image-page.component.ts
@@ -0,0 +1,78 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { OpenaiService } from '../../services/openai.service';
+import { NavBarComponent } from "../../../shared/components/nav-bar/nav-bar.component";
+@Component({
+  selector: 'app-generate-image-page',
+  standalone: true,
+  imports: [CommonModule, FormsModule, NavBarComponent],
+  templateUrl: './generate-image-page.component.html',
+  styleUrl: './generate-image-page.component.css'
+})
+export class GenerateImagePageComponent {
+    input: string = '';
+    loading:boolean = true;
+    selectedQuantity: number = 0;
+    numberArray: number[] = [];
+    images: string[] = [];
+    clicked: boolean = true;
+    constructor(private openaiService: OpenaiService) { }
+
+
+    updateNumberArray() {
+      this.numberArray = Array.from({ length: this.selectedQuantity }, (_, index) => index + 1);
+      console.log('Number array updated:', this.numberArray);
+  }
+
+  generateImage(number:string){
+    if(this.input==='') {
+      alert('Please enter a prompt');
+      return;
+    }
+    this.images = [];
+
+    this.selectedQuantity = parseInt(number, 10);
+    this.updateNumberArray();
+    this.loading = true;
+    this.openaiService.generateImage(this.input, this.selectedQuantity).subscribe((response) => {
+      console.log(response);
+      this.loading = false;
+      this.images = response.data.map((image: any) => image.url);
+      this.loading = false;
+      this.input = '';
+      this.selectedQuantity = 0;
+    },
+    (error) => {
+      console.error('Error generating image:', error);
+      this.loading = false;
+    });
+
+  }
+
+  downloadImage(imageUrl: string, index: number): void {
+    const a = document.createElement('a');
+    a.href = imageUrl;
+    a.download = `image${index + 1}.png`;
+    a.click();
+  }
+
+
+  // openInNewTab() {
+  //   if (this.imageUrl) {
+  //     window.open(this.imageUrl, '_blank');
+  //   }
+  // }
+
+  // downloadImage() {
+  //   if (this.imageUrl) {
+  //     const link = document.createElement('a');
+  //     link.href = this.imageUrl;
+  //     link.setAttribute('download', 'generated-image.png'); 
+  //     link.target = '_self'; 
+  //     link.click();
+  //   }
+  // }
+
+
+}
