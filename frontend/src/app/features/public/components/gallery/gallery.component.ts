a8e9e9cb9dfde3384c2237282fb3456f6dc13644
Kerim Begic <begick89@gmail.com>
2024-12-04 13:52:50 +0100

    task-75 added filter button for gallery

--- frontend/src/app/features/public/components/gallery/gallery.component.ts
+++ frontend/src/app/features/public/components/gallery/gallery.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 import { GetAllPhotosService } from '../../services/get-all-photos.service';
 import { OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {HttpClientModule} from '@angular/common/http';
+import _ from 'lodash';
+
 @Component({
   selector: 'app-gallery',
   standalone: true,
@@ -12,14 +13,40 @@ import {HttpClientModule} from '@angular/common/http';
 })
 export class GalleryComponent implements OnInit {
   photos: any[] = [];
-
-  constructor(private getAllPhotosService: GetAllPhotosService) { }
+  originalPhotos: any[] = []; 
+   selectedFilter: string = 'trending';
+  constructor(private getAllPhotosService: GetAllPhotosService ) { }
 
   ngOnInit() {
-    this.getAllPhotosService.getAllPhotos().subscribe((data) => {
-      this.photos = [...data];
-      //console.log(this.photos);
+    this.getAllPhotosService.getAllPhotos().subscribe((data) => { 
+      this.originalPhotos = data;
+      this.photos = this.sortByTrending(data);
+      console.log(this.photos);
     });
   }
 
+  sortByTrending(data: any) {
+    return _.orderBy(data, ['likeCount', 'viewCount'], ['desc', 'desc']);
+  }
+
+  sortByLatest(data: any) {
+    return _.orderBy(data, ['createAt'], ['desc']);
+  }
+
+  filter(event: any) {
+    this.selectedFilter = event.target.value;
+
+    if (this.selectedFilter === 'trending') {
+      this.photos = [];
+      this.photos = this.sortByTrending(this.originalPhotos);
+      console.log(this.photos);
+    }
+    else{
+      this.photos = [];
+      this.photos = this.sortByLatest(this.originalPhotos);
+      console.log(this.photos);
+    }
+    }
 }
+
+
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+import { provideAnimations } from '@angular/platform-browser/animations';
+bootstrapApplication(AppComponent, {
+  ...appConfig, // Uključi tvoju konfiguraciju aplikacije
+  providers: [
+    provideAnimations(), // Angular funkcija za animacije
+    ...(appConfig.providers || []) // Dodaj providere iz appConfig-a ako ih ima
+  ],
+}).catch((err) => console.error(err));
