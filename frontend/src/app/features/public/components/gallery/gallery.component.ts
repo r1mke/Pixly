e384c4428d600e77fa639ddc354c632458304cd8
Kerim Begic <begick89@gmail.com>
2024-12-05 23:15:23 +0100

    task-77 infinite scroll

--- frontend/src/app/features/public/components/gallery/gallery.component.ts
+++ frontend/src/app/features/public/components/gallery/gallery.component.ts
@@ -1,28 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { GetAllPhotosService } from '../../services/get-all-photos.service';
 import { OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import _ from 'lodash';
+import { InfiniteScrollModule   } from '@robingenz/ngx-infinite-scroll';
 
+
+export class AppModule {}
 @Component({
   selector: 'app-gallery',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,InfiniteScrollModule ],
   templateUrl: './gallery.component.html',
   styleUrl: './gallery.component.css'
 })
 export class GalleryComponent implements OnInit {
   photos: any[] = [];
+  totalPages : number = 0;
   originalPhotos: any[] = []; 
   selectedFilter: string = 'trending';
+  isLoading: boolean = false;
   constructor(private getAllPhotosService: GetAllPhotosService ) { }
 
-  ngOnInit() {
-    this.getAllPhotosService.getAllPhotos().subscribe((data) => { 
-      this.originalPhotos = data;
-      this.photos = this.sortByTrending(data);
-      console.log(this.photos);
-    });
+
+  ngOnInit(): void {
+    this.loadPhotos();
+  }
+
+  loadPhotos() {
+    if (this.isLoading) return;
+    this.isLoading = true;
+
+    this.getAllPhotosService.getAllPhotos().subscribe(
+    {
+      next:(res)=>{
+        console.log(res);
+        this.totalPages = res.totalPages;
+        this.originalPhotos = [...this.originalPhotos, ...res.photos];
+        this.photos = this.originalPhotos;
+        this.getAllPhotosService.incrementPageNumber();
+      },
+      error:(error) => {
+        console.error('Error fetching user:', error);
+      }, 
+      complete: () => {
+        this.isLoading = false; 
+      }
+    }  
+    )
+  }
+
+  loadMoreItems(){
+    if (this.isLoading) return;
+    console.log("load more items");
+    if(this.getAllPhotosService.getCurrentPageNumber() <= this.totalPages){
+      this.loadPhotos();
+    }
   }
 
   sortByTrending(data: any) {
@@ -46,6 +79,18 @@ export class GalleryComponent implements OnInit {
       this.photos = this.sortByLatest(this.originalPhotos);
       console.log(this.photos);
     }
+    }
+
+    @HostListener('window:scroll', ['$event'])
+    onScroll(event: Event): void {
+      const scrollPosition = window.scrollY;
+      const windowHeight = window.innerHeight;
+      const documentHeight = document.documentElement.scrollHeight;
+  
+      // Provjera je li korisnik došao do 90% stranice
+      if (scrollPosition + windowHeight >= documentHeight * 0.9) {
+        this.loadMoreItems();
+      }
     }
 }
 
--- frontend/src/app/features/public/services/get-all-photos.service.ts
+++ frontend/src/app/features/public/services/get-all-photos.service.ts
@@ -2,16 +2,31 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MYCONFIG } from '../../../my-config';
 import { HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class GetAllPhotosService {
+  pageNumber : number = 1;
+  pageSize : number = 10;
 
- 
+
   constructor(private http: HttpClient) { }
+ 
+  getAllPhotos(pageNumber : number = this.pageNumber, pageSize : number = this.pageSize): Observable<any> {
+      return this.http.get<any>(`${MYCONFIG.apiUrl}/api/photos/page/${pageNumber}/${pageSize}`);
+  }
 
-  getAllPhotos() {
-    return this.http.get<any[]>(`${MYCONFIG.apiUrl}/api/photos`);
+  incrementPageNumber() {
+    this.pageNumber++;
+  }
+
+  getCurrentPageNumber() {
+    return this.pageNumber;
+  }
+
+  getPageSize() {
+    return this.pageSize;
   }
   
 }
