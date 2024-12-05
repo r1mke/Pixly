a1b15388b928d341fe0059c5c80b453fa5a47d44
Elmir Mujkic <elmir.mujkic.online@gmail.com>
2024-12-05 16:09:28 +0100

    show user img

--- backend/backend/Endpoints/UserEndpoints/UserGetByUsername.cs
+++ backend/backend/Endpoints/UserEndpoints/UserGetByUsername.cs
@@ -19,6 +19,7 @@ namespace backend.Endpoints.UserEndpoints
                     FirstName = u.FirstName,
                     LastName = u.LastName,
                     Username = u.Username,
+                    ProfileImgUrl = u.ProfileImgUrl,
                 })
                 .FirstOrDefaultAsync();
 
