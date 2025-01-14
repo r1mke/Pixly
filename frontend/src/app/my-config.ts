3047d403f27de1c57ccdb7bc7cc2e234690104ab
r1mke <kerim.begic@edu.fit.ba>
2025-01-14 13:16:49 +0100

    updated gitignore

--- frontend/src/app/my-config.ts
+++ frontend/src/app/my-config.ts
@@ -1,4 +1,4 @@
 export const MYCONFIG = {
   apiUrl: process.env['MYAPI'],
-  openAi : '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'
+  openAi : 'API-KEY'
 };
